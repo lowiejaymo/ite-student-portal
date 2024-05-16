0fc7220a148b7910c8b9de4864aed18ea497ce34
Lifte15 <c09651052069@gmail.com>
2024-05-16 15:49:51 +0800

    Bulk

--- indexes/officer-add-student-bulk.php
+++ indexes/officer-add-student-bulk.php
@@ -18,20 +18,29 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 if (isset($_POST['save_excel_data'])) {
+
+    // Get uploaded file information
     $fileName = $_FILES['import_file']['name'];
     $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
 
+    // Define allowed file extensions
     $allowed_ext = ['xls', 'csv', 'xlsx'];
 
+    // Check if file extension is allowed
     if (in_array($file_ext, $allowed_ext)) {
+        
+        // Get temporary file path
         $inputFileNamePath = $_FILES['import_file']['tmp_name'];
 
         try {
-            /** Load $inputFileName to a Spreadsheet object **/
+            // Load Excel file into a Spreadsheet object
             $spreadsheet = IOFactory::load($inputFileNamePath);
             $data = $spreadsheet->getActiveSheet()->toArray();
 
+            // Iterate through each row of data
             foreach ($data as $row) {
+
+                // Sanitize and extract data from each row
                 $accountnumber = htmlspecialchars($row[0]);
                 $lastname = htmlspecialchars($row[1]);
                 $firstname = htmlspecialchars($row[2]);
@@ -42,19 +51,34 @@ if (isset($_POST['save_excel_data'])) {
                 $email = filter_var($row[7], FILTER_SANITIZE_EMAIL);
                 $phonenumber = htmlspecialchars($row[8]);
 
+                // Convert the names to proper case
+                $lastname = ucwords(strtolower($lastnameNotProper));
+                $firstname = ucwords(strtolower($firstnameNotProper));
+                $middlename = ucwords(strtolower($middlenameNotProper));
+                
+                // Remove the spaces of the last name
+                $lastnameremovespace = str_replace(' ', '', $lastname);
 
+                // Set the password and hashed it
                 $defaultpassword = $lastname . $accountnumber;
                 $defaulthashed_pass = password_hash($defaultpassword, PASSWORD_BCRYPT);
 
+                // Get the first letter of the first name
                 $first_letter = substr($firstname, 0, 1);
 
+                // Get the first letter of the middle name
                 $first_letter_middlename = substr($middlename, 0, 1);
+                
+                // Generate the code
                 $code = strtoupper($lastname . " , " . $firstname . " " . $first_letter_middlename . ". - " . $accountnumber . " - " . $program);
 
+                // Generate username
                 $username = strtolower($first_letter) . strtolower($lastname);
                 
+                // Set role to Student
                 $role = "Student";
                 
+                // Get the username of the admin who enrolled the student
                 $enrolled_by = $_SESSION['username'];
 
                 // Insert new student
@@ -62,10 +86,12 @@ if (isset($_POST['save_excel_data'])) {
                                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 $stmt_newstudent_query = mysqli_prepare($conn, $sql_newstudent_query);
 
+                
                 if ($stmt_newstudent_query) {
                     mysqli_stmt_bind_param($stmt_newstudent_query, "ssssssssssssss", $accountnumber, $code, $defaulthashed_pass, $username, $role, $lastname, $firstname, $middlename, $gender, $email, $phonenumber, $enrolled_by, $yearlevel, $program);
                     $result_newstudent_query = mysqli_stmt_execute($stmt_newstudent_query);
 
+                    // Redirect based on the result of the SQL query
                     if (!$result_newstudent_query) {
                         header("Location: ../officer-student-addnew.php?newStudentError=Failed to add new student account");
                         exit();
