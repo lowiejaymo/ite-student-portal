df3c7a6074fc18eac46255c6f0d2b51185a2673b
Lifte15 <c09651052069@gmail.com>
2024-06-17 18:48:14 +0800

    bulk

--- indexes/officer-add-student-bulk.php
+++ indexes/officer-add-student-bulk.php
@@ -10,7 +10,7 @@ Overview: This file handles the import of student data from an Excel file, valid
 */
 
 session_start();
-require ('db_conn.php');
+require('db_conn.php');
 require '../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -21,14 +21,20 @@ use Endroid\QrCode\Writer\PngWriter;
 
 if (isset($_POST['save_excel_data'])) {
 
+    function validate($data)
+                {
+                    $data = trim($data); // Remove whitespace from the beginning and end of string
+                    $data = stripslashes($data); // Remove backslashes
+                    $data = htmlspecialchars($data); // Convert special characters to HTML entities
+                    return $data;
+                }
+
     // Get uploaded file information
     $fileName = $_FILES['import_file']['name'];
     $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
 
     // Define allowed file extensions
     $allowed_ext = ['xls', 'csv', 'xlsx'];
-
-    
 
     // Check if file extension is allowed
     if (in_array($file_ext, $allowed_ext)) {
@@ -49,14 +55,14 @@ if (isset($_POST['save_excel_data'])) {
                 }
 
                 // Sanitize and extract data from each row
-                $accountnumber = htmlspecialchars($row[0]);
-                $lastnameNotProper = htmlspecialchars($row[1]);
-                $firstnameNotProper = htmlspecialchars($row[2]);
-                $middlenameNotProper = htmlspecialchars($row[3]);
-                $program = htmlspecialchars($row[4]);
-                $yearlevel = htmlspecialchars($row[5]);
-                $gender = htmlspecialchars($row[6]);
-                $phonenumberdefault = htmlspecialchars($row[7]);
+                $accountnumber = validate($row[0]);
+                $lastnameNotProper = validate($row[1]);
+                $firstnameNotProper = validate($row[2]);
+                $middlenameNotProper = validate($row[3]);
+                $program = validate($row[4]);
+                $yearlevel = validate($row[5]);
+                $gender = validate($row[6]);
+                $phonenumberdefault = validate($row[7]);
                 $phonenumber = "0". $phonenumberdefault;
 
                 // Convert the names to proper case
@@ -67,7 +73,7 @@ if (isset($_POST['save_excel_data'])) {
                 // Remove the spaces of the last name
                 $lastnameremovespace = str_replace(' ', '', $lastname);
 
-                // Set the password and hashed it
+                // Set the password and hash it
                 $defaultpassword = $lastnameremovespace . $accountnumber;
                 $defaulthashed_pass = password_hash($defaultpassword, PASSWORD_BCRYPT);
 
@@ -100,28 +106,48 @@ if (isset($_POST['save_excel_data'])) {
                 // Get the username of the admin who enrolled the student
                 $enrolled_by = $_SESSION['username'];
 
-                // Insert new student
-                $sql_newstudent_query = "INSERT INTO user (account_number, code, password, username, role, last_name, first_name, middle_name, gender, phone_number, enrolled_by, year_level, program)
-                                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $stmt_newstudent_query = mysqli_prepare($conn, $sql_newstudent_query);
+                // Check if the account number already exists
+                $sql_check_account = "SELECT account_number FROM user WHERE account_number = ?";
+                $stmt_check_account = mysqli_prepare($conn, $sql_check_account);
+                mysqli_stmt_bind_param($stmt_check_account, "s", $accountnumber);
+                mysqli_stmt_execute($stmt_check_account);
+                $result_check_account = mysqli_stmt_get_result($stmt_check_account);
 
-                
-                if ($stmt_newstudent_query) {
-                    mysqli_stmt_bind_param($stmt_newstudent_query, "sssssssssssss", $accountnumber, $qrcode, $defaulthashed_pass, $username, $role, $lastname, $firstname, $middlename, $gender, $phonenumber, $enrolled_by, $yearlevel, $program);
-                    $result_newstudent_query = mysqli_stmt_execute($stmt_newstudent_query);
+                if (mysqli_num_rows($result_check_account) > 0) {
+                    // Account number exists, update the record
+                    $sql_update_student = "UPDATE user SET code = ?, password = ?, username = ?, role = ?, last_name = ?, first_name = ?, middle_name = ?, gender = ?, phone_number = ?, enrolled_by = ?, year_level = ?, program = ?
+                                            WHERE account_number = ?";
+                    $stmt_update_student = mysqli_prepare($conn, $sql_update_student);
+                    mysqli_stmt_bind_param($stmt_update_student, "sssssssssssss", $qrcode, $defaulthashed_pass, $username, $role, $lastname, $firstname, $middlename, $gender, $phonenumber, $enrolled_by, $yearlevel, $program, $accountnumber);
+                    $result_update_student = mysqli_stmt_execute($stmt_update_student);
 
-                    // Redirect based on the result of the SQL query
-                    if (!$result_newstudent_query) {
-                        header("Location: ../officer-student-addnew.php?newStudentError=Failed to add new student account");
+                    if (!$result_update_student) {
+                        header("Location: ../officer-student-addnew.php?newStudentError=Failed to update student account");
                         exit();
                     }
+
                 } else {
-                    header("Location: ../officer-student-addnew.php?newStudentError=SQL preparation failed");
-                    exit();
+                    // Account number does not exist, insert new student
+                    $sql_newstudent_query = "INSERT INTO user (account_number, code, password, username, role, last_name, first_name, middle_name, gender, phone_number, enrolled_by, year_level, program)
+                                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    $stmt_newstudent_query = mysqli_prepare($conn, $sql_newstudent_query);
+
+                    if ($stmt_newstudent_query) {
+                        mysqli_stmt_bind_param($stmt_newstudent_query, "sssssssssssss", $accountnumber, $qrcode, $defaulthashed_pass, $username, $role, $lastname, $firstname, $middlename, $gender, $phonenumber, $enrolled_by, $yearlevel, $program);
+                        $result_newstudent_query = mysqli_stmt_execute($stmt_newstudent_query);
+
+                        if (!$result_newstudent_query) {
+                            header("Location: ../officer-student-addnew.php?newStudentError=Failed to add new student account");
+                            exit();
+                        }
+                    } else {
+                        header("Location: ../officer-student-addnew.php?newStudentError=SQL preparation failed");
+                        exit();
+                    }
                 }
             }
 
-            header("Location: ../officer-students.php?newStudentSuccess=New student accounts enrolled successfully");
+            header("Location: ../officer-students.php?newStudentSuccess=Student accounts created successfully");
             exit();
 
         } catch (Exception $e) {
